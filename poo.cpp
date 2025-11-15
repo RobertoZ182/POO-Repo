5995c0d9872f35a3552444d69b9c425170a77797
Roberto <rkyuchukov18@gmail.com>
2025-11-15 19:01:18 +0200

    adaugare constructori fiecare clasa

--- poo.cpp
+++ poo.cpp
@@ -12,6 +12,35 @@ public:
     const string tip;
     string *ingrediente;
 
+    Meniu() : tip("Necunoscut") {
+        denumire = "Necunoscut";
+        pret = 0;
+        nrIngrediente = 0;
+        ingrediente = nullptr;
+        totalProduse++;
+    }
+
+   
+    Meniu(string den, float p) : tip("Standard") {
+        denumire = den;
+        pret = p;
+        nrIngrediente = 2;
+        ingrediente = new string[nrIngrediente];
+        ingrediente[0] = "Apa";
+        ingrediente[1] = "Sare";
+        totalProduse++;
+    }
+
+  
+    Meniu(string den, float p, string tipM, int n) : tip(tipM) {
+        denumire = den;
+        pret = p;
+        nrIngrediente = n;
+        ingrediente = new string[n];
+        for (int i = 0; i < n; i++)
+            ingrediente[i] = "Ingredient " + to_string(i + 1);
+        totalProduse++;
+    }
     static float calculeazaReducere(float pret, int procent)
     {
         return pret - (pret * procent / 100.0f);
@@ -48,6 +77,35 @@ public:
     const int codID;
     string *mese;
 
+    Chelner() : codID(0) {
+        nume = "Anonim";
+        varsta = 0;
+        nrMese = 0;
+        mese = nullptr;
+        totalChelneri++;
+    }
+
+    
+    Chelner(string n, int v) : codID(100) {
+        nume = n;
+        varsta = v;
+        nrMese = 2;
+        mese = new string[nrMese];
+        mese[0] = "Masa 1";
+        mese[1] = "Masa 2";
+        totalChelneri++;
+    }
+
+    
+    Chelner(string n, int v, int cod, int nr) : codID(cod) {
+        nume = n;
+        varsta = v;
+        nrMese = nr;
+        mese = new string[nr];
+        for (int i = 0; i < nr; i++)
+            mese[i] = "Masa " + to_string(i + 1);
+        totalChelneri++;
+    }
     static float calculeazaBacsis(float totalVanzari, float procent)
     {
         return totalVanzari * procent / 100.0f;
@@ -83,6 +141,38 @@ public:
     static int totalComenzi;
     const string dataComenzii;
     string *produse;
+
+
+        Comanda() : dataComenzii("N/A") {
+        nrComanda = 0;
+        valoareTotala = 0;
+        nrProduse = 0;
+        produse = nullptr;
+        totalComenzi++;
+    }
+
+   
+    Comanda(int nr, float val) : dataComenzii("24/10/2025") {
+        nrComanda = nr;
+        valoareTotala = val;
+        nrProduse = 2;
+        produse = new string[nrProduse];
+        produse[0] = "Apa plata";
+        produse[1] = "Pizza";
+        totalComenzi++;
+    }
+
+
+    Comanda(int nr, float val, string data, int n) : dataComenzii(data) {
+        nrComanda = nr;
+        valoareTotala = val;
+        nrProduse = n;
+        produse = new string[n];
+        for (int i = 0; i < n; i++)
+            produse[i] = "Produs " + to_string(i + 1);
+        totalComenzi++;
+    }
+
     static float adaugaTVA(float valoare, float tva)
     {
         return valoare + valoare * tva / 100.0f;
